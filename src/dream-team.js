22dc8239a4798f4e842d28d49a12753a57d35f04
anastakozz <shvedagon@gmail.com>
2023-04-14 15:27:56 +0300

    fix: dreram team edit

--- src/dream-team.js
+++ src/dream-team.js
@@ -20,16 +20,33 @@ function createDreamTeam(arr) {
 
 
   let names = arr.filter(item => typeof item === 'string');
+  // console.log(names[0]);
+  
 
  const teamBuilder = () => {
-//   const names2 = names.map(element => {
-//    element.split().filter(letter => letter !== " ").join()
-//   });
-  return names.map(item => item[0].toUpperCase()).sort().join("");
+  const names2 = names.map(element => element.split("").filter(letter => letter !== " ").join());
+  return names2.map(item => item[0].toUpperCase()).sort().join("");
  }
+
+ console.log(names.length === 0 ? false : teamBuilder()); 
 
   return names.length === 0 ? false : teamBuilder();
 }
+
+createDreamTeam([
+  '   William Alston ',
+  ' Paul Benacerraf',
+  '  Ross Cameron',
+  '       Gilles Deleuze',
+  '  Arda Denkel ',
+  '  Michael Devitt',
+  '  Kit Fine',
+  ' Nelson Goodman',
+  'David Kolb',
+  '   Saul Kripke',
+  '  Trenton Merricks',
+  '  Jay Rosenberg',
+])
 
 module.exports = {
   createDreamTeam
